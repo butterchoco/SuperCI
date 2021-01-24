4f53ba59d7f25a12e6152aa68dce8d5cee1e9f55
Ahmad Supriyanto <ahmad.supriyanto@ui.ac.id>
2021-01-24 13:03:56 +0700

    tes

--- util/mailer.js
+++ util/mailer.js
@@ -11,7 +11,7 @@ const transporter = nodemailer.createTransport({
 const send = ({ email, name }) => {
   const from = name && email ? `${name} <${email}>` : `${name || email}`
   const message = {
-    from: "supri.contact@gmail.com",
+    from,
     to: email,
     subject: `[PENTING] Aktivasi akun kamu sekarang`,
     text: "Selamat datang",
