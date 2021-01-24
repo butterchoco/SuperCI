00d2a7e84de6bc83a43380fa3825fe0b135cee6a
Ahmad Supriyanto <ahmad.supriyanto@ui.ac.id>
2021-01-24 09:58:49 +0700

    change email

--- util/mailer.js
+++ util/mailer.js
@@ -13,7 +13,7 @@ const send = ({ email, name, text }) => {
   const from = name && email ? `${name} <${email}>` : `${name || email}`
   const message = {
     from,
-    to: 'ahmad.supriyanto@ui.ac.id',
+    to: 'qwertyvisual.contact@gmail.com',
     subject: `New message from ${from}`,
     text,
     replyTo: from
