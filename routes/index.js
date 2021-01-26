35f0fedc11709e2a91e2ccb06a4bdece878f296d
Ahmad Supriyanto <ahmad.supriyanto@ui.ac.id>
2021-01-26 13:01:13 +0700

    remove unused line

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,4 @@
 const ActivationController = require("../controller/ActivationController");
-const AccountController = require("../controller/AccountController");
-const TestController = require("../controller/TestController");
 
 const routes = (server) => {
   server.post("/activation", ActivationController)
