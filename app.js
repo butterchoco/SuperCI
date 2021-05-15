f81606743590af0178c80ac0e3ce09ac1fc4ba70
Ahmad Supriyanto <ahmad.supriyanto@ui.ac.id>
2021-05-15 07:44:42 +0700

    Refactor config

--- app.js
+++ app.js
@@ -1,20 +1,6 @@
-var whitelist = ["*"];
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (whitelist.indexOf(req.header("Origin")) !== -1) {
-    corsOptions = { origin: true };
-  } else {
-    corsOptions = { origin: false };
-  }
-  callback(null, corsOptions);
-};
-
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
-const io = require("socket.io")(server, {
-  cors: corsOptionsDelegate,
-});
 const createSocketConnection = require("./socket.io/index");
 const router = require("./routes/index");
 const logger = require("./util/Log/logger");
@@ -24,7 +10,28 @@ const createError = require("http-errors");
 const { instrument } = require("@socket.io/admin-ui");
 const bcrypt = require("bcrypt");
 
+const corsOptions = {
+  origin: ["*"],
+};
+const io = require("socket.io")(server, {
+  cors: corsOptions,
+});
 createSocketConnection(io);
+app.use(cors(corsOptions));
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+app.use("/", router);
+app.use(logger.express);
+app.use(cookieParser());
+app.use((req, res, next) => next(createError(404)));
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  res.status(err.status || 500).render("error");
+});
 
 const saltRounds = 10;
 const password = "admin";
@@ -38,21 +45,6 @@ bcrypt.hash(password, saltRounds, function (err, hash) {
       password: hashPassword,
     },
   });
-});
-app.set("view engine", "ejs");
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname + "/public"));
-app.use("/", router);
-app.use(logger.express);
-app.use(cookieParser());
-app.use(cors(corsOptionsDelegate));
-app.use((req, res, next) => next(createError(404)));
-app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  res.status(err.status || 500).render("error");
 });
 
 module.exports = app;
